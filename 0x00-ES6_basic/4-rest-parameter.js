17e262c7dd45322392907362a3cd73cf714bb757
Mano224 <mano@example.com>
2024-10-08 07:14:39 +0300

    j

---
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -0,0 +1,4 @@
+// Modify the following function to return the number of arguments passed to it using the rest parameter syntax
+export default function returnHowManyArguments(...num){
+return num.length
+}
