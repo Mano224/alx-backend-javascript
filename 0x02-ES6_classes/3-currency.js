525ad77871c907824eb66b6d7cf83d7062369426
Mano224 <Mano224@example.com>
2024-10-21 12:40:05 +0300

    j

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,34 +1,30 @@
-export default class Currency{
-    constructor(code, name){
-        this._code = code;
-        this._name = name;
+export default class Currency {
+    constructor(code, name) {
+      this._code = code;
+      this._name = name;
     }
-    
-    get code(){
-        return this._code
+  
+    get code() {
+      return this._code;
     }
-    set code(valeu){
-        if(typeof valeu !== 'string'){
-            throw new TypeError('')
-        }
-        return this._code = valeu;
+  
+    set code(value) {
+      this._code = value;
     }
-    get name(){
-        return this._name
+  
+    get name() {
+      return this._name;
     }
-    set name(valeu){
-        if(typeof valeu !== 'string'){
-            throw new TypeError('')
-        }
-        return this._name = valeu;
+  
+    set name(value) {
+      this._name = value;
     }
-     /**
-   * Creates the full string representation of this Currency.
-   * @returns {String}
-   */
-
-    displayFullCurrency(){
-        st = `${this._name} (${this._name})`
-        return st
+  
+    /**
+     * Creates the full string representation of this Currency.
+     * @returns {String}
+     */
+    displayFullCurrency() {
+      return `${this.name} (${this.code})`;
     }
-}
+  }
