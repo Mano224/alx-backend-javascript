60bd1d9f8e76f78657a28c9eacc229d25832f9dc
Mano224 <Mano224@example.com>
2024-10-21 12:15:23 +0300

    j

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,48 @@
+class HolbertonCourse{
+    constructor(name, length, students){
+        this._name = name;
+        this._length = length;
+        this._students = students;
+    }
+    get name(){
+        return this._name;
+
+    }
+    set name(value){
+        if (typeof value !== 'string'){
+            throw new TypeError('u must enter a string')
+        }
+        else 
+        return this._name = value;
+    }
+
+
+    get length(){
+        return this._length 
+    }
+
+    set length(value){
+        if( typeof value !== "number"){
+            throw new TypeError('Length must be a number')
+        }
+        else
+        return this._length = value
+    }
+
+
+    get students(){
+        return this._students 
+    }
+
+    set students(value){
+        if(!(value instanceof Array)){
+            throw new TypeError('Students must be an array of strings')
+        }
+        if(value.every((student) => { typeof student !== 'string'})){
+            throw new TypeError('must be a string')
+        }
+        else
+        return this._length = value
+    }
+
+}
