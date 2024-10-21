94e01815e1a1c01ebb9f51df00312e6dfa8eef94
Mano224 <Mano224@example.com>
2024-10-21 15:53:10 +0300

    j

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,18 +1,20 @@
 import Building from './5-building';
 
-export default class SkyHighBuilding extends Building{
-    constructor(sqft, floors){
-        super(sqft);
-        this.floors = floors;
-    }
-    get sqft(){
-        return this._sqft;
-    }
-    get floors(){
-        return this._floors;
-    }
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this.floors = floors;
+  }
 
-    evacuationWarningMessage(){
-        return `Evacuate slowly the ${this.floors} floors`
-    }
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(value) {
+    this._floors = value;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors`;
+  }
 }
